8a2ff0f7e56b6bd5d9c81286503f7f5e29967aa1
arthur <arthurmedeiros32@gmail.com>
2023-04-05 00:12:13 -0300

    feat: add messages when received from client or server

--- server.py
+++ server.py
@@ -18,10 +18,15 @@ try:
     # Cria um buffer para armazenar o arquivo recebido
     received_data = b""
 
+    client_fixed_addr = None
+
     # Recebe o arquivo do cliente em pedaços de tamanho BUFFER_SIZE
     while True:
         # Recebe um pacote do cliente
         data, addr = sock.recvfrom(BUFFER_SIZE)
+        # Armazena o endereço do cliente se ainda não foi armazenado
+        if client_fixed_addr is None:
+            client_fixed_addr = addr
         # Adiciona o pacote recebido ao dado recebido
         received_data += data
         # Se o pacote recebido tiver menos que BUFFER_SIZE bytes, é o último pacote
@@ -30,17 +35,20 @@ try:
 
     # Salva o arquivo recebido
     with open("received_server_" + filename, "wb") as f:
-        f.write(received_data)
+        extraMessageClient = "-> received from client"
+        f.write(received_data + extraMessageClient.encode())
 
     # Abre o arquivo recebido e envia ele de volta para o cliente em pedaços de tamanho BUFFER_SIZE
-    with open("received_server_" + filename, "rb") as f:
+    with open(filename, "rb") as f:
         data = f.read(BUFFER_SIZE)
         while data:
-            sock.sendto(data, addr)
+            extraMessageServer = " -> received from server"
+            sock.sendto(data + extraMessageServer.encode(), client_fixed_addr)
             data = f.read(BUFFER_SIZE)
 
 except socket.timeout:
     print(f"Tempo limite de {TIMEOUT_LIMIT} segundos atingido. Encerrando conexão...")
 
 # Fecha o socket
+print('Conexão encerrada.')
 sock.close()
