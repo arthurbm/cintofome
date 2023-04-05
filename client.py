35505d1e4b0ef225c698d44b5daa3d63b68988ca
arthur <arthurmedeiros32@gmail.com>
2023-04-04 23:55:10 -0300

    feat: add constants and deal with timeout

--- client.py
+++ client.py
@@ -1,17 +1,14 @@
 import socket
-
-# Endereço IP e porta do servidor
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-
-# Tamanho do buffer em bytes
-BUFFER_SIZE = 1024
+from constants import BUFFER_SIZE, TIMEOUT_LIMIT, UDP_IP, UDP_PORT
 
 # Nome do arquivo que será enviado
 filename = "example.txt"
 
 # Cria o socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Define um tempo limite para receber uma resposta do servidor (em segundos)
+sock.settimeout(TIMEOUT_LIMIT)
 
 # Envia o nome do arquivo para o servidor
 sock.sendto(filename.encode(), (UDP_IP, UDP_PORT))
@@ -29,12 +26,17 @@ with open(filename, "rb") as f:
 # Recebe o arquivo de volta do servidor
 received_data = b""
 while True:
-    # Recebe um pacote do servidor
-    data, addr = sock.recvfrom(BUFFER_SIZE)
-    # Adiciona o pacote recebido ao dado recebido
-    received_data += data
-    # Se o pacote recebido tiver menos que BUFFER_SIZE bytes, é o último pacote
-    if len(data) < BUFFER_SIZE:
+    try:
+        # Recebe um pacote do servidor
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+        # Adiciona o pacote recebido ao dado recebido
+        received_data += data
+        # Se o pacote recebido tiver menos que BUFFER_SIZE bytes, é o último pacote
+        if len(data) < BUFFER_SIZE:
+            break
+    except socket.timeout:
+        # Se o tempo limite expirar, exibe uma mensagem de erro e sai do loop
+        print(f"Tempo limite de {TIMEOUT_LIMIT} segundos atingido. Encerrando conexão...")
         break
 
 # Salva o arquivo recebido
---
+++ constants.py
@@ -0,0 +1,6 @@
+# Endereço IP e porta que o servidor irá escutar
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+# Tamanho do buffer em bytes
+BUFFER_SIZE = 1024
+TIMEOUT_LIMIT = 10
