bcc4400f16d64073493f32c950b353323dd1a74e
Júnior Marcondes <marc@mm-2.local>
2023-04-23 16:52:52 -0300

    pf

--- aux_functions.py
+++ aux_functions.py
@@ -11,15 +11,15 @@ class Packet:
         self.checksum = checksum
 
     def make_packet(self):
-        checksum = self.real_checksum(self.data.encode())
-        return (str(self.seq_n) + "|" + str(self.checksum) + "|" + self.data).encode()
+        _checksum = self.real_checksum()
+        return (str(self.seq_n) + "|" + str(_checksum) + "|" + self.data).encode()
 
     # TODO: implement is_ack, checksum and is_corrupt
     def is_ACK(self):
         return (self.seq_n == "" and self.data == "")
 
     def real_checksum(self):
-        data = self.make_packet()
+        data = self.make_packet().encode()
 
         polynomial = 0x1021
         crc = 0xFFFF
