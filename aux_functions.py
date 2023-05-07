ea3086a70fe0257200b8cc8ded2c234f690dc8b8
arthur <arthurmedeiros32@gmail.com>
2023-05-07 18:44:15 -0300

    [part-3] feat: make part 3 modifications from lucas branch

--- aux_functions.py
+++ aux_functions.py
@@ -52,13 +52,13 @@ def extract_packet(string_packet):
     return Packet(int(seq_num), is_ack, data, checksum=int(checksum_))
 
 def send_packet(sock, packet, addr):
-    print(f"Enviando pacote: {packet.seq_n} de")
-    print(" " + str(len(packet.make_packet().encode('utf-8'))) + " bytes")
+    # print(f"Enviando pacote: {packet.seq_n} de")
+    # print(" " + str(len(packet.make_packet().encode('utf-8'))) + " bytes")
     sock.sendto(packet.make_packet().encode(), addr)
 
 def send_ack(sock, seq_num, addr):
     packet = Packet(seq_num, True, 0, 0)
-    print(f"Enviando ACK: {seq_num}")
+    # print(f"Enviando ACK: {seq_num}")
     sock.sendto(packet.make_packet().encode(), addr)
 
 # def make_ack_packet(seq_num):
@@ -70,15 +70,80 @@ def wait_for_ack(sock, expected_ack):
         data, _ = sock.recvfrom(BUFFER_SIZE - ack.reading_size())
         ack = extract_packet(data)
         if ack.is_ack and expected_ack == ack.seq_n:
-            print(f"ACK recebido: {ack.seq_n}")
+            # print(f"ACK recebido: {ack.seq_n}")
             return True
         else:
-            print(f"ACK incorreto: {ack.seq_n}, esperado: {expected_ack}")
+            # print(f"ACK incorreto: {ack.seq_n}, esperado: {expected_ack}")
             # exit()
             return False
     except socket.timeout:
-        print(f"Tempo limite de {TIMEOUT_LIMIT} segundos atingido.")
+        # print(f"Tempo limite de {TIMEOUT_LIMIT} segundos atingido.")
         return False
 
 def packet_loss(probability):
     return random.random() < probability
+
+def chunk_divide(data, head_size):
+    data_bytes = data.encode('utf-8')
+
+    chunk_size = BUFFER_SIZE - head_size
+
+    chunks = [data_bytes[i:i+chunk_size] for i in range(0, len(data_bytes), chunk_size)]
+
+    return chunks
+
+def sock_receive(sock, expected_seq_num):
+    received_data = "".encode('utf-8')
+
+    while True:
+        try:
+           # Recebe um pacote 
+            data, addr = sock.recvfrom(BUFFER_SIZE)
+            packet = extract_packet(data)
+            # filename = packet.data
+            if packet.seq_n == expected_seq_num:
+                # Avalia o checksum dele
+                if packet.checksum ==  packet.real_checksum():
+                    # print(f"Pacote recebido: {packet.seq_n}")
+                    send_ack(sock, packet.seq_n, addr)
+                    expected_seq_num = 1 - expected_seq_num
+                    received_data += packet.data.encode('utf-8')
+                    if len(packet.data) + packet.reading_size() < BUFFER_SIZE:
+                        break
+                else:
+                    # print(f"Checksum incorreto: {packet.checksum}, esperado: {packet.real_checksum()}")
+                    send_ack(sock, 1 - expected_seq_num, addr)
+            else:
+                # print(f"Pacote incorreto: {packet.seq_n}, enviando ACK anterior")
+                send_ack(sock, 1 - expected_seq_num, addr)
+        except socket.timeout:
+            # print(
+                # f"Tempo limite de {TIMEOUT_LIMIT} segundos atingido. Encerrando conexão...")
+            data = ""
+            addr = ""
+            break
+
+    return data, expected_seq_num, addr 
+
+def sock_send(message, sock, seq_num, addr):
+
+    packet = Packet(seq_num, False, "")
+
+    chunks = chunk_divide(message, packet.reading_size()) 
+
+    for data in chunks:
+        while True:
+            # Envia o pedaço de arquivo para o cliente usando rdt3.0
+            packet = Packet(seq_num, False, data.decode('utf-8'))
+            # # print(f"Enviando pacote {seq_num}")
+            send_packet(sock, packet, addr)
+            ack_received = wait_for_ack(sock, seq_num)
+            if ack_received:
+                seq_num = 1 - seq_num
+
+                packet = Packet(seq_num, False, "")
+                break
+            else:
+                print()
+    
+    return seq_num
---
+++ utils.py
@@ -0,0 +1,9 @@
+#cardapio
+menu={
+  1: ('nhoque', '5.00'),
+  2: ('escondidinho', '10.00'),
+  3: ('parmegiana', '12.00'),
+  4: ('lasanha vegana', '15.00')
+}
+#opcoes
+options = "1 - cardápio\n2 - pedir \n3 - conta individual\n4 - conta da mesa\n5 - pagar\n6 - levantar"
